1536186e055c07f0a66e03ab2c6ea00344201208
surya <121973463+surya391@users.noreply.github.com>
2025-06-29 19:47:57 +0530

    Update axiosInstance.jsx

--- client/src/utils/axiosInstance.jsx
+++ client/src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL : 'http://localhost:3001/api'
+    baseURL : 'https://pet-adoption-system-iz2r.onrender.com'
 })
 
 export default axiosInstance
