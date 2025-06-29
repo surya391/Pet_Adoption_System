d2a3149927bf800a4984b141e33c2db0baf5c76e
surya <121973463+surya391@users.noreply.github.com>
2025-06-29 19:32:20 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const configureDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/18-pet');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('Connected to the database');
   } catch (error) {
     console.error('Error connecting to the database:', error);
